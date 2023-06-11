c4f29d52d84a7d7de00a00fa9da34906e0ab7383
Fenrir <126280190+darshan019@users.noreply.github.com>
2023-06-11 23:17:49 +0530

    a quicksort algorithm

---
+++ Assignment2/quicksort.js
@@ -0,0 +1,25 @@
+//A quicksort algorithm made for practicing recursion
+
+function sort(arr){
+    if(arr.length < 2){
+        return arr
+    }
+    let pivot = arr[arr.length - 1]
+    
+    let left = []
+    let right = [pivot]
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] < pivot){
+            left.push(arr[i])
+        }
+    }
+    for(let j = 0; j < arr.length; j++){
+        if(arr[j] > pivot){
+            right.push(arr[j])
+        }
+    }
+    return [...sort(left), ...sort(right)]
+    
+}
+
+console.log(sort([10,5,74,69,25,45,62]))
